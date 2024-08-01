004f062ee7de1559da503d3049c95248132b6a51
Jakob Meier <mail@jakobmeier.ch>
2024-08-01 16:59:30 +0200

    simple per page access statistics

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@
 import { serve } from "bun";
 import { build_options, env } from "./env.js";
 import handler from "./handler.js";
+import { initPageCount, countPageLoads } from "./loadCount.js";
 
 const hostname = env("HOST", "0.0.0.0");
 const port = parseInt(env("PORT", 3000));
 
 const { httpserver, websocket } = handler(build_options.assets ?? true);
 
+await initPageCount();
+
 const serverOptions = {
   baseURI: env("ORIGIN", undefined),
-  fetch: httpserver,
+  // fetch: async ,httpserver
+  async fetch(req, srv) {
+    countPageLoads(req.url);
+    return await httpserver(req, srv);
+  },
   hostname,
   port,
   development: env("SERVERDEV", build_options.development ?? false),
---
+++ src/loadCount.js
@@ -0,0 +1,62 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { env } from "./env.js";
+
+
+let pageLoads = {};
+const dataFolder = env("ACCESS_STATS", "access-stats");
+const intervalSeconds = Number(env("STATS_INTERVAL", "3600"));
+const latestFile = path.join(dataFolder, "latest.json");
+
+export async function initPageCount() {
+  if (!await fs.exists(dataFolder)) {
+    await fs.mkdir(dataFolder);
+  }
+
+  await restorePageLoads();
+
+  setInterval(exportPageLoads, intervalSeconds * 1000);
+}
+
+export function countPageLoads(url) {
+  pageLoads[url] = (pageLoads[url] || 0) + 1;
+}
+
+
+
+async function restorePageLoads() {
+  try {
+    const data = await fs.readFile(latestFile, "utf-8");
+    pageLoads = JSON.parse(data);
+    console.log("Restored page loads:", pageLoads);
+  } catch (error) {
+    console.error("Error reading latest.json. Start counting at zero.", error);
+    pageLoads = {};
+  }
+}
+
+
+async function exportPageLoads() {
+  const now = new Date();
+  const fileName = path.join(
+    dataFolder,
+    `${now.toISOString().split("T")[0]}.json`
+  );
+
+  // Read the existing data for today, if any
+  let dailyData = [];
+  try {
+    const existingData = await fs.readFile(fileName, "utf-8");
+    dailyData = JSON.parse(existingData);
+  } catch (error) {
+    console.log("No existing daily data, creating new.");
+  }
+
+  // Add a data record for today
+  dailyData.push({ timestamp: now.toISOString(), pageLoads });
+  await fs.writeFile(fileName, JSON.stringify(dailyData, null, 2));
+
+  // Update latest for restoration
+  await fs.writeFile(latestFile, JSON.stringify(pageLoads, null, 2));
+  console.log(`Exported page loads to ${fileName}`);
+}
